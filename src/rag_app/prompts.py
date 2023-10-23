ee7c4fb2dae4e516f929bbf30efcf5172a3f360c
AshishSinha5 <ashish97.kumar@gmail.com>
2023-10-23 11:54:48 +0530

    added comments

--- src/rag_app/prompts.py
+++ src/rag_app/prompts.py
@@ -1,7 +1,5 @@
 
-
-
-
+# Using the retriaval qa prompt template from the langchain library - https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/chains/retrieval_qa/prompt.py
 def create_prompt(query, relevant_docs):
     relevant_text = ''
     relevant_docs = relevant_docs['documents'][0]
@@ -11,10 +9,6 @@ def create_prompt(query, relevant_docs):
     prompt = f"""Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 {relevant_text}
 Question: {query} 
-Provide the steps in the following format and remove all mentions of figures and tables:
-1 - ...
-2 - ...
-Steps:
 Helpful Answer:"""
     
     return prompt
