4042b6db52f5da8b6048f0a5dfed680182204519
AshishSinha5 <ashish97.kumar@gmail.com>
2023-10-23 11:55:22 +0530

    added util function for reading txt and yaml files

--- src/rag_app/utils.py
+++ src/rag_app/utils.py
@@ -1,5 +1,13 @@
 import json
 import pickle
+import yaml
 
-def read_file(file):
-    pass
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        data = f.read()
+    return data
+
+def load_yaml_file(path):
+    with open(path, 'r') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    return data
